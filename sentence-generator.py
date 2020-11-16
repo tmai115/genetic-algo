f2bcf735948ab445e626e42499d5f5f695447dc9
Tracey Mai <tmai468@aucklanduni.ac.nz>
2020-11-16 21:20:09 +1300

    Create random samples of populations of individuals

--- sentence-generator.py
+++ sentence-generator.py
@@ -8,3 +8,35 @@ import string
 seed = 123
 np.random.seed(seed)
 random.seed(seed)
+
+# Class to represent genetic algo, with method to initialise population
+class GeneticAlgorithm:
+	def __init__(self, fitness_function, num_attributes = 2, population_size = 100,
+				crossover_prob = .75, mutation_prob = .05):
+		self.fitness_function = fitness_function
+		self.num_attributes = num_attributes
+		self.population_size = population_size
+		self.crossover_prob = crossover_prob
+		self.mutation_prob = mutation_prob
+		self.population = None
+		self.population_avg_score = 0
+		self.fitness_scores = None
+		self.fittest_individuals = None
+
+	def initialise_population(self):
+		"""init a population of individuals
+		args:
+			num_attributes: length of each individual (attributes)
+			population_size: number of individuals
+		returns:
+			population_size lists of n length each.
+		"""
+		attributes = []
+
+		for attribute in range(self.num_attributes):
+			attributes.append(
+				np.random.choice(
+					list(string.punctuation + string.ascii_letters +
+						string.whitespace),
+					size = self.population_size))
+		self.population = np.array(attributes).T
