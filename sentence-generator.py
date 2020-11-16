a88c9daf02178daf15c860c9d773cf3a1f01f60f
Tracey Mai <tmai468@aucklanduni.ac.nz>
2020-11-16 21:17:07 +1300

    Initialise population and generate random sample of individuals

---
+++ sentence-generator.py
@@ -0,0 +1,10 @@
+# A system using a genetic algorithm to generate a target sentence
+import random
+import matplotlib.pyplot as plt 
+import numpy as np 
+import string
+
+# Fixing seed of reproducibility
+seed = 123
+np.random.seed(seed)
+random.seed(seed)
